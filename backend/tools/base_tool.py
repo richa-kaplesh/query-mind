989b77807b2f6215c10cec397c9b1c918a41f63e
richa-kaplesh <kapleshricha@gmail.com>
2026-06-22 14:31:54 +0530

    file changes

---
+++ backend/tools/base_tool.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class BaseTool(ABC):
+    
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+    
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        pass
+    
+    @abstractmethod
+    def run(self, query: str) -> dict:
+        pass
